fd824aa42735d1121c34237ec5776c60b22aeced
Alex Allafi <alexallafi.dev@gmail.com>
2022-03-21 13:41:55 +0200

    Fix .gitignore for builds on another machine

--- app/src/main/java/com/pesimatik/appbox/ui/UsageToSizeDispatcher.kt
+++ app/src/main/java/com/pesimatik/appbox/ui/UsageToSizeDispatcher.kt
@@ -4,7 +4,7 @@ object UsageToSizeDispatcher {
     fun calculateFontSizes(usages: Map<String, Int>): Map<String, Float> {
         val globalFontSize = 15f
         val usagesSum = usages.values.sum()
-        return usages.mapValues { globalFontSize + (globalFontSize * it.value.toFloat() / usagesSum) + (globalFontSize * it.value.toFloat() / usagesSum) * 3 }
+        return usages.mapValues { globalFontSize + (globalFontSize * it.value.toFloat() / usagesSum) * 4 }
     }
 
     fun calculateFontSizes(activity: String, usages: Map<String, Int>): Float {
